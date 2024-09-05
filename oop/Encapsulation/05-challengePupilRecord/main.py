98d316eb1d7bfaf005b66ee8c035ee573aa74278
Cerecero <ale.rey.cer@outlook.com>
2024-09-05 17:03:16 -0600

    added comments

--- oop/Encapsulation/05-challengePupilRecord/main.py
+++ oop/Encapsulation/05-challengePupilRecord/main.py
@@ -1,22 +1,30 @@
 class Student:
     def __init__(self, name):
-        self.name = name # instance variable name equals name parameter
-        self.__courses = {} #empty dictionary
+        self.name = name  # instance variable name equals name parameter
+        self.__courses = {}  # empty dictionary
 
     def calculate_letter_grade(self, score):
-        if score >= 90: #if score is above 90 return A
+        if score >= 90:  # if score is above 90 return A
             return "A"
-        elif score >= 80 and score < 90: # if score is above 80 and less than 89 return B
+        elif score >= 80 and score < 90:  # if score is above 80 and less than 89 return B
             return "B"
-        elif score >= 70 and score < 80: # if score is above 70 and less than 79 return C
+        elif score >= 70 and score < 80:  # if score is above 70 and less than 79 return C
             return "C"
-        elif score >= 60 and score < 70: # if score is above 60 and less than 69 return D
+        elif score >= 60 and score < 70:  # if score is above 60 and less than 69 return D
             return "D"
-        else: #else return F
+        else:  # else return F
             return "F"
-    def add_course(self, course_name, score): 
-        self.__courses[course_name] = Student.calculate_letter_grade(self, score)
-        #Add to diciontary __courses the key [course_name] and the value calculated_letter_grade
-    def get_courses(self):
-        return self.__courses #returns the dictionary
 
+    def add_course(self, course_name, score):
+        self.__courses[course_name] = Student.calculate_letter_grade(
+            self, score)
+        # Add to diciontary __courses the key [course_name] and the value calculated_letter_grade
+
+        # other solutions
+        # letter_grade = self.calculate_letter_grade(score)
+        # self.__courses[course_name] = letter_grade
+
+        # self.__courses[course_name] = self.calculate_letter_grade(score)
+
+    def get_courses(self):
+        return self.__courses  # returns the dictionary
