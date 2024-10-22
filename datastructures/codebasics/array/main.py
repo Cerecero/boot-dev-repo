7c839f01aa943ae42fd4095a9d4afcbd191098d6
Cerecero <ale.rey.cer@outlook.com>
2024-10-22 11:05:34 -0600

    added folder codebaiscs

---
+++ datastructures/codebasics/array/main.py
@@ -0,0 +1,33 @@
+expenses = [
+    {'january': 2200},
+    {'february': 2350},
+    {'march': 2600},
+    {'april': 2130},
+    {'may':2190}
+]
+
+# print(expenses[0]['january']-expenses[1]['february'])
+# suma = 0
+# for expense in expenses[:3]:
+#     suma += sum(expense.values())
+#
+# for exp in expenses:
+#     if exp.values() == 2000:
+#         print('true')
+#     print('false')
+# expenses.append({'june':1900})
+# print(expenses)
+#
+# expenses[3]['april'] = 1930
+# print(expenses)
+# 1. In Feb, how many dollars you spent extra compare to January?
+    #150
+# 2. Find out your total expense in first quarter (first three months) of the year.
+    #7150
+# 3. Find out if you spent exactly 2000 dollars in any month
+    #false
+# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+    
+# 5. You returned an item that you bought in a month of April and
+# got a refund of 200$. Make a correction to your monthly expense list
+# based on this
